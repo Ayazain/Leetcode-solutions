b4e8fd0d36faf5233b5e82ddf147865455cc332f
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-08-26 04:30:49 +0200

    Time: 128 ms (73.21%), Space: 40.2 MB (97.26%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cs
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cs
@@ -15,28 +15,18 @@ public class Solution {
       bool check(TreeNode node , double RIGHT  , double LEFT)
     {
           if(node == null ) return true  ; 
-          
-             Console.WriteLine(RIGHT+"<"+ node.val+"<" +LEFT) ;
-                       Console.WriteLine(RIGHT<node.val) ;
-                                 Console.WriteLine(node.val<LEFT) ;
-
-
           if(node.val<=RIGHT || node.val>=LEFT)
           {
-                        
               return false ;
-          }
-          int val  = node.val; 
-            Console.WriteLine(val);
-         return  check(node.right ,  node.val , LEFT)&check(node.left , RIGHT , node.val) ; 
+          }           
+         return  check(node.left , RIGHT , node.val)&check(node.right ,  node.val , LEFT) ; 
        
     }
     public bool IsValidBST(TreeNode root)
     {
         bool valid = true;
         
-         if (root.left == null && root.right == null)
-            return true;
+       
    valid=  check(root , double.NegativeInfinity, double.PositiveInfinity );
         return valid;
     }

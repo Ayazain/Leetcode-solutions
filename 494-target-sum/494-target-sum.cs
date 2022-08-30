9d7f473c65164a13ba4d1b63d7796e4ab2f2cbd0
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-08-30 17:01:27 +0200

    Time: 1138 ms (19.10%), Space: 38.2 MB (93.20%) - LeetHub

--- 494-target-sum/494-target-sum.cs
+++ 494-target-sum/494-target-sum.cs
@@ -1,17 +1,20 @@
 public class Solution {
- public int Count = 0;
-    public int FindTargetSumWays(int[] nums, int target) {
-       Dfs(0,0, target, nums);
-       return Count;
+     int max =0 ; 
+    public int FindTargetSumWays(int[] nums, int target) 
+    {
+        DFS(  0 , 0 , target , nums );
+        return max ; 
     }
-    public void Dfs(int i, int result, int target, int[] nums) {
-        if(i == nums.Length) {
-            if(result == target)
-                Count++;
-            return;
-        }
-        Dfs(i+1, result + nums[i], target, nums);
-        Dfs(i+1, result - nums[i], target, nums);
+     private void  DFS(int index , int sum , int target,int[] nums  )
+    {
+       if (index == nums.Length)
+       {
+           if (sum  == target )
+               max++ ; 
+           return  ; 
+       }
+          DFS(  index+1 ,  sum+nums[index] ,  target  , nums );
+          DFS(  index+1 ,  sum-nums[index] ,  target , nums );
     }
 
 }

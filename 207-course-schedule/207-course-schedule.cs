9f6637256da2be70f7270af4e5d691ab03246f7a
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-09-01 21:49:25 +0200

    Time: 231 ms (9.18%), Space: 43.2 MB (49.49%) - LeetHub

--- 207-course-schedule/207-course-schedule.cs
+++ 207-course-schedule/207-course-schedule.cs
@@ -2,40 +2,50 @@ public class Solution {
     int []haveEdges  ; 
    bool []REmoved   ; 
  Stack<int> nodes = new Stack<int>();
+      Dictionary<int, List<int>> Mynodes;
 Queue<int> order = new Queue<int>();
  
     public bool CanFinish(int numCourses, int[][] prerequisites) {
-         haveEdges = new int[numCourses]; 
-    REmoved = new bool[numCourses];
-    for (int i = 0 ; i < prerequisites.Length ; i++)
-        haveEdges[prerequisites[i][0]]++;
-     for(int i = 0 ; i < numCourses ; i++)
+          Mynodes = new Dictionary<int, List<int>>();
+        haveEdges = new int[numCourses];
+        REmoved = new bool[numCourses];
+        for (int i = 0; i < numCourses; i++)
         {
-            if(haveEdges[i]== 0)
-           
-            { 
-                order.Enqueue(i); 
-               
-            }
+            Mynodes.Add(i, new List<int>());
         }
-        
-        while(order.Count>0)
+        for (int i = 0; i < prerequisites.Length; i++)
         {
-            
-            int Eelement =  order.Dequeue();
-             Console.WriteLine(Eelement);
-            REmoved[Eelement]= true; 
-             for (int i = 0 ; i < prerequisites.Length ; i++)
-               if(prerequisites[i][1] == Eelement)
-                 {
-                     haveEdges[prerequisites[i][0]]--;
-                     if(haveEdges[prerequisites[i][0]]== 0)
-                      order.Enqueue(prerequisites[i][0]); 
-                 }
+            Mynodes[prerequisites[i][1]].Add(prerequisites[i][0]);
         }
-bool canfinish = true; ;
-for (int i = 0 ; i < REmoved.Length ; i++)
-        if(!REmoved[i]) canfinish = false;
-   return canfinish ;        
+        foreach (var item in Mynodes)
+        {
+            if (item.Value.Count == 0)
+                order.Enqueue(item.Key);
+
+        }
+        for (int i = 0; i < prerequisites.Length; i++)
+            haveEdges[prerequisites[i][0]]++;
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (haveEdges[i] == 0)
+                order.Enqueue(i);
+        }
+        while (order.Count > 0)
+        {
+            int Eelement = order.Dequeue();
+            REmoved[Eelement] = true;
+             
+            for (int i = 0; i < Mynodes[Eelement].Count; i++)
+                {
+                    haveEdges[ Mynodes[Eelement][i]]--;
+                    if (haveEdges[ Mynodes[Eelement][i]] == 0)
+                        order.Enqueue( Mynodes[Eelement][i]);
+                }
+                
+        }
+        bool canfinish = true; ;
+        for (int i = 0; i < REmoved.Length; i++)
+            if (!REmoved[i]) canfinish = false;
+        return canfinish;      
     }
 }

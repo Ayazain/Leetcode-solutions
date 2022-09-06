348a237c2be2ff56a7b3fd37f83a8c604599cf14
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-09-06 20:48:36 +0200

    Time: 210 ms (45.31%), Space: 38.2 MB (44.48%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cs
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int LengthOfLIS(int[] nums) {
          int []dp = new int [nums.Length];
+        int max = 0  ; 
     for (int i = nums.Length-2 ; i >=0 ; i--)
     {
         for (int j = i+1; j <= nums.Length-1 ; j++)
@@ -8,8 +9,10 @@ public class Solution {
             if(nums[i]<nums[j])
                dp[i]= Math.Max(dp[j]+1, dp[i]);  
         }
+        if(dp[i]> max)
+            max= dp[i];
     }
 
-    return dp.Max()+1;    
+    return max+1;    
     }
 }

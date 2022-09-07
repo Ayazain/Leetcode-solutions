cd41e6b0b93b29a7d44a3787fbb6374c504ada30
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-09-07 15:57:40 +0200

    Time: 27 ms (82.75%), Space: 25.4 MB (42.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cs
+++ 62-unique-paths/62-unique-paths.cs
@@ -1,22 +1,29 @@
 public class Solution {
   public   int UniquePaths(int m, int n) {
-     int [][]dp = new int[m][]; 
+   int [][]dp = new int[m][]; 
      for (int i = 0; i < m; i++)
      {
         dp[i]= new int[n];
      }
      dp[m-1][n-1]= 1 ; 
-     for (int i = m-1 ; i >=0 ; i--)
-     {
-        for (int j = n-1; j >=0; j--)
+      for(int row = 0; row < m; row++)
         {
-            if(i<m-1)
-            dp[i][j] +=  dp[i+1][j];
-            if(j<n-1)
-            dp[i][j] += dp[i][j+1];
+            dp[row][n-1] = 1;
+        }
+        
+        for (int col = 0; col < n; col++)
+        {
+            dp[m-1][col] = 1;
+        }
+     for (int i = m-2 ; i >=0 ; i--)
+     {
+        for (int j = n-2; j >=0; j--)
+        {
+            dp[i][j] +=  dp[i+1][j]+dp[i][j+1];
+           
         }
      }
      
-        return  dp[0][0] ; 
-    }
+        return  dp[0][0] ;  
+}
 }

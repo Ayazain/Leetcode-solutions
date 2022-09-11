3be971cf6125a55e362a0db32cbf56638ad9327d
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-09-12 00:01:41 +0200

    Time: 187 ms (89.12%), Space: 51.2 MB (12.60%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cs
+++ 56-merge-intervals/56-merge-intervals.cs
@@ -1,17 +1,20 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-  if (intervals == null || intervals.Length == 0) return new List<int []>().ToArray();
+     if (intervals == null || intervals.Length == 0) return new List<int []>().ToArray();
         Array.Sort(intervals ,(a,b)=>a[0].CompareTo(b[0]) );
          List<int[]> MergedInterval  =   new List<int[]>() ; 
+         
+            int index = 0  ; 
          for (int i = 0  ; i < intervals.Length ;i++) 
+         { 
+          if(i == 0 ) {MergedInterval.Add(intervals[i]); index++; continue ; }
+          if( MergedInterval[index-1][1]>= intervals[i][0])
+             MergedInterval[index-1][1] = Math.Max(MergedInterval[index-1][1], intervals[i][1]);
+         else 
          {
-            int index = 0  ;  
-          while (index < MergedInterval.Count &&MergedInterval[index][1]< intervals[i][0]) index++;
-          if (index >=MergedInterval.Count ) 
-          MergedInterval.Add(intervals[i]);
-          else 
-          MergedInterval[index][1] = Math.Max(MergedInterval[index][1], intervals[i][1]);
+             MergedInterval.Add(intervals[i]); index++;}
          }
-         return MergedInterval.ToArray();       
+         return MergedInterval.ToArray();
+    
     }
 }

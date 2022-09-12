78bd1e230139fcc1fe7d55df3a540f2e8d0bc888
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-09-12 21:04:14 +0200

    Time: 250 ms (12.67%), Space: 38.2 MB (52.65%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cs
@@ -0,0 +1,56 @@
+public class Solution {
+      public static bool compare(Dictionary<char, int> truechar, Dictionary<char, int> chare)
+    {
+        bool ret = true;
+        foreach (KeyValuePair<char, int> x in truechar)
+        {
+            if (x.Value > chare[x.Key])
+                return false;
+        }
+        return ret;
+    }
+    public string MinWindow(string t, string s) {
+      Dictionary<char, int> chars = new Dictionary<char, int>();
+        Dictionary<char, int> truechars = new Dictionary<char, int>();
+        int TotalLen = int.MaxValue; //  a new  Minimum 
+        int Current = 0; //Length of sliding window 
+        int FinalStart = 0;
+        for (int i = 0; i < s.Length; i++)// creating the twe dictionary 
+        {
+            if (truechars.ContainsKey(s[i]))
+            {
+                truechars[s[i]]++;
+            }
+            else
+            {
+                chars.Add(s[i], 0);
+                truechars.Add(s[i], 1);
+            }
+        }
+        int start = 0; int end = 1; //ADOBECODEBANCDDD
+        for (end = 0; end < t.Length; end++)
+        {
+            if (chars.ContainsKey(t[end]))
+            {
+                chars[t[end]]++;
+            }
+            Current++;
+            while (compare(truechars , chars))
+            {
+                if (TotalLen > Current)// ubdate Length 
+                {
+                    TotalLen = Current;
+                    FinalStart = start;
+                }
+                if (chars.ContainsKey(t[start]))
+                {
+                    chars[t[start]]--;
+                }
+                Current--;
+                start++;
+            }
+        }
+        if (TotalLen == int.MaxValue) return "";
+        return t.Substring(FinalStart, TotalLen);
+    }
+}

e999bf622481ed40cfbf499c38512594df200693
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-09-24 16:12:47 +0200

    Time: 909 ms (24.87%), Space: 50.5 MB (5.34%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cs
+++ 567-permutation-in-string/567-permutation-in-string.cs
@@ -1,18 +1,27 @@
 public class Solution {
-   public bool CheckInclusion(string s1, string s2) {
-          if (s1.Length > s2.Length) return false;
-
-        char[] s1sorted = s1.ToCharArray();
-        Array.Sort(s1sorted);
-        string s1SortedString = new String(s1sorted);
+     public bool compare(string s2, Dictionary<char, int> lookup)
+    {
+        for (int i = 0; i < s2.Length; i++)
+        {
+            if (!lookup.ContainsKey(s2[i])) return false;
+            lookup[s2[i]]--;
+            if (lookup[s2[i]] < 0) return false;
+        }
+        return true;
+    }
+    public bool CheckInclusion(string s1, string s2)
+    {
+        if (s1.Length > s2.Length) return false;
+        Dictionary<char, int> lookup = new Dictionary<char, int>();
+      
+        for (int i = 0; i < s1.Length; i++)
+            if (lookup.ContainsKey(s1[i])) lookup[s1[i]]++;
+            else lookup.Add(s1[i], 1);
 
         for (int i = 0; i <= s2.Length - s1.Length; i++)
         {
-            char[] s2sorted = s2.Substring(i, s1.Length).ToCharArray();
-            Array.Sort(s2sorted);
-            string s2SortedString = new String(s2sorted);
-            if (s1SortedString.CompareTo(s2SortedString) == 0)
-                return true;
+            string str = s2.Substring(i, s1.Length);
+           if (compare(str, new Dictionary<char, int>(lookup))) return true;
         }
         return false;
     }

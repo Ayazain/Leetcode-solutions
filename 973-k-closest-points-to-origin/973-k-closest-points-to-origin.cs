86b8fcf503f9aa819ace4c277aaade823a2a826f
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-10-08 10:00:54 +0200

    Time: 361 ms (91.71%), Space: 57.8 MB (12.43%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cs
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cs
@@ -1,23 +1,18 @@
 public class Solution {
-     private double distance(int v1, int v2)
-    {
-        return Math.Abs((v1*v1) +(v2*v2));
-    }
-
-    public int[][] KClosest(int[][] points, int k)
+ public int[][] KClosest(int[][] points, int k)
     {
         PriorityQueue<int, double> myqueue = new PriorityQueue<int, double>(points.Length + 1);
         int[][] res = new int[k][];
-        int index,  j = 0;
+        int index; 
         for (int i = 0; i < points.Length; i++)
         {
-            myqueue.Enqueue(i, distance(points[i][0], points[i][1]));
+            myqueue.Enqueue(i, Math.Abs((points[i][0]*points[i][0]) +(points[i][1]*points[i][1])));
         }
         while (k > 0)
         {
             index = myqueue.Dequeue();
-            res[j] = points[index];
-            k--; j++;
+            res[res.Length-k] = points[index];
+            k--; 
         }
         return res  ; 
     }

7baf627437492ffa0827819f16a4e5c5ccb24fa4
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-10-09 21:42:38 +0200

    Time: 291 ms (45.62%), Space: 41.9 MB (27.08%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cs
+++ 621-task-scheduler/621-task-scheduler.cs
@@ -1,27 +1,26 @@
 public class Solution {
     public int LeastInterval(char[] tasks, int n) {
+       
         if (n == 0 ) return tasks.Count() ;
-          Dictionary<char, int> lookUp = new Dictionary<char, int>();
-        PriorityQueue<char, int> myQueue; 
+       int[] counter  = new int [26]; int max = 0 , maxCounter =0 ;  
         for (int i = 0; i < tasks.Length; i++) // fill dic with num of rebetetion
         {
-            if (lookUp.ContainsKey(tasks[i])) lookUp[tasks[i]]++;
-            else lookUp.Add(tasks[i], 1);
-        }
-        myQueue = new PriorityQueue<char, int>(lookUp.Count);
-        foreach (var x in lookUp)
-        {
-            myQueue.Enqueue(x.Key, 1 - x.Value);
-        }
-        char c  =  myQueue.Dequeue() ; 
-        int Count  = lookUp[c]; int looklikeMe = 0  ; 
-        while (myQueue.Count>0&&lookUp[myQueue.Dequeue()]== Count) looklikeMe++;
-        int res  = ((n+1)*(Count-1)+1) ; //total for now 
-        res+= looklikeMe ;
-        int temp = res -Count ;// rest of me 
-        if (tasks.Length-Count > temp)
-              res += tasks.Length-Count -temp ; 
-        
-        return res ; 
+          counter[tasks[i]-'A']++; 
+          if ( counter[tasks[i]-'A'] ==  max) maxCounter++; 
+          else if  (counter[tasks[i]-'A'] > max)
+          { 
+            max = counter[tasks[i]-'A']  ;
+             maxCounter=1; }
+          }
+     
+       
+       
+        int res  = ((n+1)*(max-1)+1) ; //total for now 
+          res+= maxCounter-1 ;
+          int temp = res -max ;// rest of me 
+          if (tasks.Length-max > temp)
+              res += tasks.Length-max -temp ; 
+      
+          return res; 
     }
 }

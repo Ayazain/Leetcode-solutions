467cac83d411b5d3d4b6eb4ac55115587dd632e4
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-10-14 18:55:36 +0200

    Time: 62 ms (18.00%), Space: 26.2 MB (34.67%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cs
@@ -0,0 +1,48 @@
+public class Solution {
+    public int TotalNQueens(int n) {
+     SolveNQueens(n) ;
+        return res.Count;
+    }
+    public  List<IList<string>> res  = new List<IList<string>>();
+ public  List<string> All = new List<string>() ; 
+  public  IList<IList<string>> SolveNQueens(int n) {
+        Char []arr =  new char[n];   
+       for(int i = 0 ; i < n  ; i++ )
+          {
+            arr[i]='.';
+          }
+          for (int i = 0 ; i < n ; i++)
+          {
+            arr[i]='Q'; 
+            All.Add( new String(arr)); 
+            arr[i] = '.';
+          }
+
+  SolveNQueensHelpper(new int [n] , n-1 ,  new List<string>()  , new Dictionary<int, bool>() , new Dictionary<int, bool>()  );
+  return res ;
+    }
+
+public  void SolveNQueensHelpper(int [] colomns  , int row  ,  List<string> curr , Dictionary<int , bool >NDigonal  , Dictionary<int ,bool>PDigonal )
+{
+    if ( row < 0 )
+    { 
+        res.Add(new List<string>(curr)); 
+    }
+    for (int i = colomns.Length-1  ; i  >=0 ; i--)
+    {
+        if (colomns[i] != -1 &&!NDigonal.ContainsKey(row-i) && !PDigonal.ContainsKey(row+i))
+        {
+         colomns[i]=-1;
+        curr.Add(new String( All[i])) ; 
+        NDigonal.Add(row-i , true );
+        PDigonal.Add(row+i, true ); 
+        SolveNQueensHelpper(colomns , row-1 , curr, NDigonal , PDigonal) ; 
+        curr.RemoveAt(curr.Count-1);
+        NDigonal.Remove(row-i );
+        PDigonal.Remove(row+ i );
+        colomns[i] = 0; 
+        } 
+    }
+    return ; 
+}
+}

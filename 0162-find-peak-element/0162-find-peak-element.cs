4c522e528acf6e618af836d3d1247f8f99092a16
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-10-15 20:57:23 +0200

    Time: 94 ms (88.03%), Space: 38.9 MB (9.80%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cs
@@ -0,0 +1,26 @@
+public class Solution {
+public   int FindPeakElement(int[] nums) {
+       if (nums.Length == 1 ) return  0 ;
+        int res = FindPeakElementHelpper(nums ,0 , nums.Length-1 );
+         return res ; 
+    }
+
+    private  int FindPeakElementHelpper(int[] nums, int s, int e)
+    {
+      if (s>e) return -1 ;
+      int mid = s+((e-s)/2); 
+      bool right = false ;  bool left = false ; 
+      if (mid-1 < 0  || nums[mid-1]< nums[mid] )  right= true ;
+      if ( mid+1 > nums.Length -1||  nums[mid+1]< nums[mid] ) left = true ; 
+        if (left & right) return  mid ; 
+       else if  (right)  
+          return  FindPeakElementHelpper(nums ,mid+1 ,e  ); 
+        else if (left)
+        {
+          return  FindPeakElementHelpper(nums ,s ,mid-1  ); 
+        }  
+        else 
+         return  FindPeakElementHelpper(nums ,mid+1 ,e  ); 
+       
+    }
+}

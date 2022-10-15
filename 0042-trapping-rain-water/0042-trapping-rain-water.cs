2089291481fcfd02f78288dcf41b1e4f7eb77ad8
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-10-16 00:18:17 +0200

    Time: 176 ms (37.48%), Space: 39.8 MB (53.64%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cs
@@ -0,0 +1,22 @@
+public class Solution {
+ public  int Trap(int[] height) {
+        int []PrevMax = new int [height.Length]; int prev = 0  ;  int next = 0  ; 
+         int []NextMax = new int [height.Length];
+         int res = 0 ; 
+         for (int i = 0  ; i < height.Length ; i++)
+         {
+          if (height[i]> prev ) { prev = height[i] ;} PrevMax[i] = prev; 
+          if (height[height.Length-i-1]> next ) { next = height[height.Length-1-i] ;} NextMax[height.Length-i-1] = next; 
+         }
+         for (int i = 0 ; i < height.Length ; i++)
+         {
+          if (i>0&&i<height.Length-1&& height[i] < PrevMax[i-1] && height[i]<NextMax[i])
+          {
+             int min = Math.Min(PrevMax[i-1] , NextMax[i+1]); 
+              res += min -height[i];
+          }
+         }
+         return res  ; 
+    }
+
+}

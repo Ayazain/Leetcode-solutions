5b7de88b95178c2583ae9059840f99fa1a0738b9
Ayazain <58406402+Ayazain@users.noreply.github.com>
2022-11-16 01:37:25 +0200

    Time: 121 ms (96.94%), Space: 50.2 MB (18.97%) - LeetHub

--- 0155-min-stack/0155-min-stack.cs
+++ 0155-min-stack/0155-min-stack.cs
@@ -1,47 +1,36 @@
-class StackElemnt 
-    {
-    int val ; int min  ; 
-  public StackElemnt(int val , int min )
-    {
-      this.val  = val ; 
-      this.min = min ; 
-    }
-    public int GetMin()
-    {
-      return min ; 
-    }
-    public int GetVal()
-    {
-      return val; 
-    }
-    }
-   public class MinStack {
+public class MinStack {
       
-    Stack<StackElemnt> myvalues = new Stack<StackElemnt>() ; 
- 
+    Stack<int > myvalues ; 
+    Stack<int > myMin ; 
+    
     public MinStack() {
-        myvalues = new Stack<StackElemnt>() ; 
+        myvalues = new Stack<int >() ; 
+        myMin = new Stack<int>(); 
     }
     
-    public void Push(int val) {  
-        int minval = val ; 
-      if (myvalues.Count() != 0 ) 
-       minval = Math.Min (GetMin() , val); 
-      StackElemnt  elemnt = new StackElemnt(val , minval) ; 
-      myvalues.Push(elemnt); 
+    public void Push(int val) {
+  
+      if (myvalues.Count() == 0 ||GetMin() >=  val) 
+         {
+         myMin.Push(val);  
+         }
+      myvalues.Push(val); 
     }
     
-    public void Pop() {
-        myvalues.Pop(); 
+   public void Pop() {
+      int removedVal =   myvalues.Pop(); 
+      if (GetMin() == removedVal) 
+      myMin.Pop();
     }
+    
     
     public int Top() {
-         StackElemnt elemnt  =  myvalues.Peek(); 
-         return elemnt.GetVal(); 
+         return myvalues.Peek(); 
     }
     
     public int GetMin() {
-       StackElemnt minElemnt  =  myvalues.Peek(); 
-       return minElemnt.GetMin(); 
+    
+       int  min  =  myMin.Peek(); 
+       return min; 
     }
 }
